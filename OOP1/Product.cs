21c52379e0dc4c8172311da6bffb66463f39eef4
Nasuh Onur Yaşa <onur.yasa@monovi.com.tr>
2021-12-07 20:21:21 +0300

    Yazılım  Geliştirme Kampı

---
+++ OOP1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Product
+    {
+        //snippet
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        //ürün ismi
+        public string ProductName { get; set; }
+        //ürün birim fiyatı
+        public double UnitPrice { get; set; }
+        //ürün stok adedi
+        public int UnitsInStock { get; set; }
+
+    }
+}
