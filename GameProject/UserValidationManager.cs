fdd3bd5a85d18e185f5e13ab0e48753a0802c6bc
Nasuh Onur Yaşa <onur.yasa@monovi.com.tr>
2021-12-18 01:14:31 +0300

    GameProject-Engın-Demırog

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+   public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear==1997 && gamer.FirstName=="NASUH ONUR"
+                &&gamer.LastName=="YAŞA"&&gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
